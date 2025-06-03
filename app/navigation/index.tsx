44bf8c3c7b26e7b79dcf711711a2ec49dea78230
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 00:24:23 +0300

    Update index.tsx

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,6 +1,8 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import RoleSwitch from '../screens/RoleSwitch';
+import ApplicantScreen from '../screens/ApplicantScreen';  // Экран соискателя
+import EmployerScreen from '../screens/EmployerScreen';    // Экран работодателя
 
 const Stack = createNativeStackNavigator();
 
@@ -9,6 +11,8 @@ export default function Navigation() {
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
         <Stack.Screen name="RoleSwitch" component={RoleSwitch} />
+        <Stack.Screen name="ApplicantScreen" component={ApplicantScreen} />
+        <Stack.Screen name="EmployerScreen" component={EmployerScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
