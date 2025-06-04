b6636b64c2ccee6ac573228d7eb8babdaf5d94df
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 14:21:02 +0300

    Update Applicants.tsx

--- app/screens/Applicants.tsx
+++ app/screens/Applicants.tsx
@@ -1,8 +1,10 @@
+import React from 'react';
 import { View, Text } from 'react-native';
-export default function Applicants() {
+
+export default function ApplicantScreen() {
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Applicants</Text>
+    <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
+      <Text>Экран Соискателя</Text>
     </View>
   );
 }
