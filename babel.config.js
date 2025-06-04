f23afd6b52750791299122098bfa32bd3c52eb6b
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 15:58:32 +0300

    Update babel.config.js

--- babel.config.js
+++ babel.config.js
@@ -1,10 +1,13 @@
+// babel.config.js  (в корне репозитория)
 module.exports = function (api) {
   api.cache(true);
+
   return {
     presets: ['babel-preset-expo'],
     plugins: [
-      ['@babel/plugin-transform-private-methods', { loose: false }],
-      ['@babel/plugin-proposal-private-property-in-object', { loose: false }],
+      ['@babel/plugin-transform-class-properties', { loose: true }],
+      ['@babel/plugin-transform-private-methods',      { loose: true }],
+      ['@babel/plugin-proposal-private-property-in-object', { loose: true }],
     ],
   };
 };
