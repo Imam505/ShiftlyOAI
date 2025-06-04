608806322a648ff4bee0fb3587eecdb3a1dff1a0
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 15:14:40 +0300

    Update HomeSeeker.tsx

--- app/screens/HomeSeeker.tsx
+++ app/screens/HomeSeeker.tsx
@@ -1,10 +1,11 @@
+// app/screens/HomeSeeker.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function HomeSeeker() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HomeSeeker</Text>
+      <Text>Экран соискателя</Text>
     </View>
   );
 }
