3e1c0bc1b8f74fd623f28f6f1cd3eecc598109c2
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 15:14:06 +0300

    Update index.tsx

--- app/navigation/index.tsx
+++ app/navigation/index.tsx
@@ -1,20 +1,27 @@
+// app/navigation/index.tsx
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-import RoleSwitch from '../screens/RoleSwitch';
-import ApplicantScreen from '../screens/HomeSeeker';
-import EmployerScreen from '../screens/HomeEmployer';
+import RoleSwitch     from '../screens/RoleSwitch';
+import HomeSeeker     from '../screens/HomeSeeker';
+import HomeEmployer   from '../screens/HomeEmployer';
 
-const Stack = createNativeStackNavigator();
+export type RootStackParamList = {
+  RoleSwitch   : undefined;
+  HomeSeeker   : undefined;
+  HomeEmployer : undefined;
+};
+
+const Stack = createNativeStackNavigator<RootStackParamList>();
 
 export default function Navigation() {
   return (
     <NavigationContainer>
       <Stack.Navigator screenOptions={{ headerShown: false }}>
-        <Stack.Screen name="RoleSwitch" component={RoleSwitch} />
-        <Stack.Screen name="ApplicantScreen" component={ApplicantScreen} />
-        <Stack.Screen name="EmployerScreen" component={EmployerScreen} />
+        <Stack.Screen name="RoleSwitch"   component={RoleSwitch}   />
+        <Stack.Screen name="HomeSeeker"   component={HomeSeeker}   />
+        <Stack.Screen name="HomeEmployer" component={HomeEmployer} />
       </Stack.Navigator>
     </NavigationContainer>
   );
