cd0388d04fb2de092a51cb70df5271d87786723b
Imam505 <gebekovimam1999@gmail.com>
2025-06-04 15:14:52 +0300

    Update HomeEmployer.tsx

--- app/screens/HomeEmployer.tsx
+++ app/screens/HomeEmployer.tsx
@@ -1,10 +1,11 @@
+// app/screens/HomeEmployer.tsx
 import React from 'react';
 import { View, Text } from 'react-native';
 
 export default function HomeEmployer() {
   return (
     <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>HomeEmployer</Text>
+      <Text>Экран работодателя</Text>
     </View>
   );
 }
